79c79be2fb567ec2177569065c3781913fed0182
rom1c <280385556@qq.com>
2021-06-05 00:46:26 +0800

    完成条件分页查询,并且把分页的拦截器放在mall-service-dependency服务之中创建StartConfig之中

--- mall-service/mall-goods-service/src/main/java/com/example/mall/MallGoodsApplication.java
+++ mall-service/mall-goods-service/src/main/java/com/example/mall/MallGoodsApplication.java
@@ -1,27 +1,13 @@
 package com.example.mall;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.example.mall.mapper"})
 public class MallGoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MallGoodsApplication.class,args);
-    }
-
-    /***
-     * 分页拦截器
-     */
-    @Bean
-    public PaginationInterceptor paginationInterceptor(){
-        PaginationInterceptor paginationInterceptor=new PaginationInterceptor();
-        //设置数据库类型
-        paginationInterceptor.setDbType(DbType.MYSQL);
-        return paginationInterceptor;
     }
 }
---
+++ mall-util/mall-service-dependency/src/main/java/com/example/mall/StartConfig.java
@@ -0,0 +1,21 @@
+package com.example.mall;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StartConfig {
+
+    /****
+     * 分页拦截器
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        PaginationInterceptor pageInterceptor = new PaginationInterceptor();
+        // 设置数据类型
+        pageInterceptor.setDbType(DbType.MYSQL);
+        return pageInterceptor;
+    }
+}
