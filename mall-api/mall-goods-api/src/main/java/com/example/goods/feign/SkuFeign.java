086983bc37fef29da296b0380382cd30920a413a
chenqi <280385556>
2021-06-17 15:48:10 +0800

    添加feign给外部服务调用

---
+++ mall-api/mall-goods-api/src/main/java/com/example/goods/feign/SkuFeign.java
@@ -0,0 +1,26 @@
+package com.example.goods.feign;
+
+import com.example.goods.model.Sku;
+import com.example.util.RespResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @Author: chenq
+ * @Description:
+ * @Date: Created in 2021-06-17 13:52
+ */
+@FeignClient("mall-goods")
+@RequestMapping("/sku")
+public interface SkuFeign {
+    @GetMapping("/aditems/type")
+    public List<Sku> typeItems(@RequestParam("id") Integer id);
+
+    @DeleteMapping("/aditems/type")
+    public RespResult delTypeItems(@RequestParam("id") Integer id);
+
+    @PutMapping("/aditems/type")
+    public RespResult<List<Sku>> updateTypeItems(@RequestParam("id") Integer id);
+}
