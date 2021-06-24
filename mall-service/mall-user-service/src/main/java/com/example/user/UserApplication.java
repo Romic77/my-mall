66db22456a6e34b5e8f8512e153ec6dbd57c24e0
chenqi <280385556>
2021-06-24 20:11:41 +0800

    订单下单功能

--- mall-service/mall-user-service/src/main/java/com/example/user/UserApplication.java
+++ mall-service/mall-user-service/src/main/java/com/example/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.example.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date: Created in 2021-06-24 16:17
  */
 @SpringBootApplication
+@MapperScan(basePackages = {"com.example.mapper"})
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
