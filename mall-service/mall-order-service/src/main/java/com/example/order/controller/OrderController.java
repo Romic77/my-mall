dc3ae62606b5043d4140a1fb22d2d037cb357d1b
chenqi <280385556>
2021-06-24 20:16:29 +0800

    订单下单功能

--- mall-service/mall-order-service/src/main/java/com/example/order/controller/OrderController.java
+++ mall-service/mall-order-service/src/main/java/com/example/order/controller/OrderController.java
@@ -4,10 +4,7 @@ import com.example.order.model.Order;
 import com.example.order.service.OrderService;
 import com.example.util.RespResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author: chenq
@@ -16,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController {
     @Autowired
     private OrderService orderService;
