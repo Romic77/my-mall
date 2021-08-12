71eaafd02b7f90e3a4f5e7fc1994aef0a0e3bc63
romic <280385556@qq.com>
2021-08-12 20:35:44 +0800

    限流

--- mall-gateway/mall-api-gateway/src/main/java/com/example/limit/IpKeyResolver.java
+++ mall-gateway/mall-api-gateway/src/main/java/com/example/limit/IpKeyResolver.java
@@ -1,21 +1,22 @@
 package com.example.limit;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 /**
  * 根据IP限流
  */
-@Component
 public class IpKeyResolver implements KeyResolver {
+
     /**
      * 将IP作为限流标识
      *
      * @param exchange
      * @return
      */
+    @Bean("ipKeyResolver")
     @Override
     public Mono<String> resolve(ServerWebExchange exchange) {
         return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
